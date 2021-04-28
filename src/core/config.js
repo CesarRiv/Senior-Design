f9d3f4f320faaf4b4eacdc7ebb0c998492996a2d
CesarRiv <riveracesar82@gmail.com>
2021-04-28 13:06:48 -0500

    Dashboard changes

--- src/core/config.js
+++ src/core/config.js
@@ -1,11 +1,9 @@
 // Replace with your own firebase config!
 export const FIREBASE_CONFIG = {
-  apiKey: 'AIzaSyBDsdN3XWM5D2-c_ti5j44GrhMnBHNmSF0',
-  authDomain: 'react-native-market-2861b.firebaseapp.com',
-  databaseURL: 'https://react-native-market-2861b.firebaseio.com',
-  projectId: 'react-native-market-2861b',
-  storageBucket: 'react-native-market-2861b.appspot.com',
-  messagingSenderId: '878215484396',
-  appId: '1:878215484396:web:c36074bccb0b5693b0a6c9',
-  measurementId: 'G-317PLP59P2',
+  apiKey: "AIzaSyAjHqZWSKpOHMHf6msZrnnHoP5JTkcib0E",
+  authDomain: "fire-af017.firebaseapp.com",
+  projectId: "fire-af017",
+  storageBucket: "fire-af017.appspot.com",
+  messagingSenderId: "399673199606",
+  appId: "1:399673199606:web:9c3845546d51d5ff1515d7"
 }
--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,23 +1,195 @@
-import React from 'react'
+'use strict'
+import React, { Component, useState } from 'react'
 import Background from '../components/Background'
 import Logo from '../components/Logo'
 import Header from '../components/Header'
 import Paragraph from '../components/Paragraph'
 import Button from '../components/Button'
 import { logoutUser } from '../api/auth-api'
+import { DataTable } from 'react-native-paper'
+import { TextInput } from 'react-native-gesture-handler'
+import Sentiment from 'sentiment'
 
-const Dashboard = () => (
-  <Background>
-    <Logo />
-    <Header>Let’s start</Header>
-    <Paragraph>
-      Your amazing app starts here. Open you favorite code editor and start
-      editing this project.
-    </Paragraph>
-    <Button mode="outlined" onPress={logoutUser}>
-      Logout
-    </Button>
-  </Background>
-)
+
+
+const sentiment = new Sentiment();
+const SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+recognition.constinous = true;
+recognition.interimResults = true;
+recognition.lang = 'en-US';
+
+
+
+
+
+
+class Dashboard extends Component {
+
+
+
+
+
+  constructor(props){
+    super(props);
+    this.state = {
+      sentimentScore:null,
+      generalSentiment:null,
+      listening: false
+    };
+    this.toggleListen = this.toggleListen.bind(this)
+    this.handleListen = this.handleListen.bind(this)
+    this.findSentiment = this.findSentiment.bind(this)
+  }
+
+  toggleListen(){
+    this.setState({
+      listening: !this.state.listening
+    }, this.handleListen)
+  }
+
+  handleListen() {
+
+    console.log('listening?', this.state.listening)
+
+    if (this.state.listening) {
+      recognition.start()
+      recognition.onend = () => {
+        console.log("...continue listening...")
+        recognition.start()
+      }
+
+    } else {
+      recognition.stop()
+      recognition.onend = () => {
+        console.log("Stopped listening per click")
+      }
+    }
+
+    recognition.onstart = () => {
+      console.log("Listening!")
+    }
+
+    let finalTranscript = ''
+    recognition.onresult = event => {
+      let interimTranscript = ''
+
+      for (let i = event.resultIndex; i < event.results.length; i++) {
+        const transcript = event.results[i][0].transcript;
+        if (event.results[i].isFinal) finalTranscript += transcript + ' ';
+        else interimTranscript += transcript;
+      }
+      document.getElementById('interim').innerHTML = interimTranscript
+      document.getElementById('final').innerHTML = finalTranscript
+
+    //-------------------------COMMANDS------------------------------------
+
+      const transcriptArr = finalTranscript.split(' ')
+      const stopCmd = transcriptArr.slice(-3, -1)
+      console.log('stopCmd', stopCmd)
+
+      if (stopCmd[0] === 'stop' && stopCmd[1] === 'listening'){
+        recognition.stop()
+        recognition.onend = () => {
+          console.log('Stopped listening per command')
+          const finalText = transcriptArr.slice(0, -3).join(' ')
+          document.getElementById('final').innerHTML = finalText
+        }
+      }
+    }
+    
+  //-----------------------------------------------------------------------
+    
+    recognition.onerror = event => {
+      console.log("Error occurred in recognition: " + event.error)
+    }
+
+  }
+
+  findSentiment(event){
+    const result = sentiment.analyze(event.target.value)
+    console.log(result)
+    this.setState({
+      sentimentScore: result.comparative
+    })
+    if(result.score<0) {
+      this.setState({
+        generalSentiment: 'Negative'
+      })
+    }
+    else if(result.score>0) {
+        this.setState({
+          generalSentiment: 'Positive'
+        })
+    }
+    else{
+      this.setState({
+        generalSentiment: 'Neutral'
+      })
+    }
+  
+  }
+
+  render() {
+    return(
+    <Background>
+      <Logo/>
+      <Header>Sentimental App</Header>
+      <Paragraph>
+        Please enter how your day has been so far down below.
+      </Paragraph>
+      <textarea onChange={this.findSentiment}/>
+      <p>Sentiment Score: {this.state.sentimentScore}</p>
+      <p>General Sentiment: {this.state.generalSentiment}</p>
+      <Button mode="outlined">
+          Submit Entry
+        </Button>
+      <div style={container}>
+        <button id='microphone-btn' style={button} onClick={this.toggle} />
+        <div id='interim' style={interim}></div>
+        <div id='final' style={final}></div>
+      </div>
+        <Button mode="outlined" onPress={logoutUser}>
+          Logout
+        </Button>
+      </Background>
+    )
+  }
+  }
 
 export default Dashboard
+
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    textAlign: 'center'
+  },
+  button: {
+    width: '60px',
+    height: '60px',
+    background: 'lightblue',
+    borderground: 'lightblue',
+    borderRadius: '50%',
+    margin: '6em 0 2em 0'
+  },
+  interim: {
+    color: 'gray',
+    border: '#ccc 1px solid',
+    padding: '1em',
+    margin: '1em',
+    width: '300px'
+  },
+  final: {
+    color: 'black',
+    border: '#ccc 1px solid',
+    padding: '1em',
+    margin: '1em',
+    width: '300px'
+  }
+}
+
+const { container,button,interim,final } = styles
